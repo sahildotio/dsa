ee42c9b47c067c35a1252bb658d9c52bfd63178f
sahilkumar-09 <9142sahilkumar@gmail.com>
2026-08-19 17:09:48 +0530

    implement sortColor function to sort an array of colors using the Dutch National Flag algorithm

---
+++ two-pointer/dutchNationFlag.js
@@ -0,0 +1,21 @@
+const sortColor = (nums) => {
+    let low = 0;
+    let mid = 0;
+    let high = nums.length - 1;
+
+    while (mid <= high) {
+        if (nums[mid] === 0) {
+            [nums[low], nums[mid]] = [nums[mid], nums[low]];
+            low++;
+            mid++;
+        } else if (nums[mid] === 1) {
+            mid++;
+        } else {
+            [nums[mid], nums[high]] = [nums[high], nums[mid]];
+            high--;
+        }
+    }
+    return nums;
+}
+
+console.log(sortColor([2, 0, 2, 1, 1, 0])); // Output: [0, 0, 1, 1, 2, 2]
